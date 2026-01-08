ebbb2ae3c32959d1a7a1f31e28ed380e2860c67b
Michael Tobing <michaelebenezerlumbantobing@gmail.com>
2026-01-08 19:55:00 +0700

    Menambahkan 3 file bentuk bentuk looping

---
+++ Struktur Kontrol Kondisional dan Perulangan/IF_ElseIF_Else_Case.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    int a = 5;
+    if (a > 0)
+    {
+        printf("angka positif");
+    } else if (a == 0)
+    {
+        printf("angka nol");
+    } else{
+        printf(" angka negatif");
+    }
+    
+    
+
+    return 0;
+}
---
+++ Struktur Kontrol Kondisional dan Perulangan/IF_Else_Case.c
@@ -0,0 +1,11 @@
+#include <stdio.h>  
+int main(){
+    int a = 5;
+    if (a < 4)
+    {
+        printf("nilai anda lebih besar dari 4\n");
+    } else{
+        printf("nilai anda lebih kecil dari 4\n");
+    }
+    return 0;
+}
